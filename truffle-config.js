f628eb742e869275be0944b4da217fa534611f59
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-29 22:49:26 -0600

    espicificando version de solc

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,15 @@
 module.exports = {
   networks: {
-    development: {      
-      host: 'localhost',
+    development: {
+      host: "localhost",
       port: 7545,
-      network_id: '*',
+      network_id: "*",
       gas: 5000000
     }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24"
+    }
   }
-}
+};
