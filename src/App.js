3633a83530f7f1a07077d5efa0c78800f8721119
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-31 23:06:33 -0600

    intanciando web y el contrato

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
 import React, { Component } from "react";
 import Panel from "./Panel";
 
+import getWeb3 from "./getWeb3";
+import AirlineContract from "./airline";
+
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: undefined
+    };
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  async componentDidMount() {
+    this.web3 = await getWeb3();
+    this.airline = await AirlineContract(this.web3.currentProvider);
+    console.log(this.airline);
+    console.log(this.web3.eth);
+    let accounts = await this.web3.eth.getAccounts();
+    console.log(accounts);
+    this.setState({ accounts }, () => this.load());
+  }
 
-    render() {
-        return <React.Fragment>
-            <div className="jumbotron">
-                <h4 className="display-4">Welcome to the Airline!</h4>
-            </div>
+  async load() {}
 
-            <div className="row">
-                <div className="col-sm">
-                    <Panel title="Balance">
+  render() {
+    return (
+      <React.Fragment>
+        <div className="jumbotron">
+          <h4 className="display-4">Welcome to the Airline!</h4>
+        </div>
 
-                    </Panel>
-                </div>
-                <div className="col-sm">
-                    <Panel title="Loyalty points - refundable ether">
-
-                    </Panel>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-sm">
-                    <Panel title="Available flights">
-
-
-                    </Panel>
-                </div>
-                <div className="col-sm">
-                    <Panel title="Your flights">
-
-                    </Panel>
-                </div>
-            </div>
-        </React.Fragment>
-    }
+        <div className="row">
+          <div className="col-sm">
+            <Panel title="Balance"></Panel>
+          </div>
+          <div className="col-sm">
+            <Panel title="Loyalty points - refundable ether"></Panel>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm">
+            <Panel title="Available flights"></Panel>
+          </div>
+          <div className="col-sm">
+            <Panel title="Your flights"></Panel>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
