e6742de64f5a70ddac5f50366393835bd37744a3
Robin Beltran <rbeltranmontijo@gmail.com>
2020-04-01 22:17:34 -0600

    obteniendo el balance y las cuentas desde metamask que se conecto con ganache

--- src/App.js
+++ src/App.js
@@ -4,25 +4,40 @@ import Panel from "./Panel";
 import getWeb3 from "./getWeb3";
 import AirlineContract from "./airline";
 
+const converter = web3 => value =>
+  web3.utils.fromWei(value.toString(), "ether");
+
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      accounts: undefined
+      accounts: undefined,
+      balance: 0
     };
   }
 
   async componentDidMount() {
     this.web3 = await getWeb3();
+    this.toEther = converter(this.web3);
     this.airline = await AirlineContract(this.web3.currentProvider);
-    console.log(this.airline);
+    // console.log(this.airline);
     console.log(this.web3.eth);
     let accounts = await this.web3.eth.getAccounts();
     console.log(accounts);
     this.setState({ accounts }, () => this.load());
   }
 
-  async load() {}
+  async getBalance() {
+    let weiBalance = await this.web3.eth.getBalance(this.state.accounts[0]);
+    console.log(weiBalance);
+    this.setState({
+      balance: this.toEther(weiBalance)
+    });
+  }
+
+  async load() {
+    this.getBalance();
+  }
 
   render() {
     return (
@@ -33,7 +48,14 @@ export class App extends Component {
 
         <div className="row">
           <div className="col-sm">
-            <Panel title="Balance"></Panel>
+            <Panel title="Balance">
+              <p>
+                <strong>{this.state.accounts}</strong>
+              </p>
+              <span>
+                <strong>Balance:</strong> {this.state.balance}
+              </span>
+            </Panel>
           </div>
           <div className="col-sm">
             <Panel title="Loyalty points - refundable ether"></Panel>
